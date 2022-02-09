439a80da43bfbc5e708e0c1ae1c96f39e13a726b
Juliet-Ol <agutjuliet@gmail.com>
2022-02-09 19:07:01 +0300

    create a form

---
+++ src/app/goal/goal.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { AlertService } from '../alert-service/alert.service';
+import { Goal } from '../goal';
+import { GoalService } from '../goal-service/goal.service';
+
+
+@Component({
+  selector: 'app-goal',
+  templateUrl: './goal.component.html',
+  styleUrls: ['./goal.component.css'],
+  providers: [GoalService]
+})
+export class GoalComponent implements OnInit {  
+
+  goals!: Goal[];
+  alertService!: AlertService;
+  
+  addNewGoal(goal: any){
+
+
+  let goalLength = this.goals.length;
+  goal.id = goalLength+1;
+  goal.completeDate = new Date(goal.completeDate)
+  this.goals.push(goal)
+}
+toggleDetails(index:any){
+  this.goals[index].showDescription = !this.goals[index].showDescription;
+}  
+
+completeGoal(isComplete: any, index: any){
+  if (isComplete) {
+    this.goals.splice(index,1);
+  }
+}
+deleteGoal(isComplete:any, index: any){
+  if (isComplete) {
+    let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`)
+    
+    if (toDelete){
+      this.goals.splice(index,1)
+      this.alertService.alertMe("The goal has been deleted")
+    }
+  }
+}
+constructor(goalService:GoalService, alertService:AlertService) {
+  this.goals = goalService.getGoals()
+  this.alertService = alertService;
+
+ }  ngOnInit(): void {
+}
+}
+
