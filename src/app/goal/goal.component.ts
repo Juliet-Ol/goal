a7dcf3ba154c774a2ca26c591c322c2c045dd1af
Juliet-Ol <agutjuliet@gmail.com>
2022-02-11 00:51:34 +0300

    add service

---
+++ src/app/goal/goal.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { AlertService } from '../alert-service/alert.service';
+import { Goal } from '../goal';
+import { GoalService } from '../goal-service/goal.service';
+import { HttpClient } from '@angular/common/http';
+
+import { Quote } from '../quote-class/quote';
+
+
+
+@Component({
+  selector: 'app-goal',
+  templateUrl: './goal.component.html',
+  styleUrls: ['./goal.component.css'],
+  providers: [GoalService]
+})
+export class GoalComponent implements OnInit {  
+
+  goals!: Goal[];
+  alertService!: AlertService;
+  quote!:Quote;
+  http: any;
+  
+  addNewGoal(goal: any){
+
+
+  let goalLength = this.goals.length;
+  goal.id = goalLength+1;
+  goal.completeDate = new Date(goal.completeDate)
+  this.goals.push(goal)
+}
+toggleDetails(index:any){
+  this.goals[index].showDescription = !this.goals[index].showDescription;
+}  
+
+completeGoal(isComplete: any, index: any){
+  if (isComplete) {
+    this.goals.splice(index,1);
+  }
+}
+deleteGoal(isComplete:any, index: any){
+  if (isComplete) {
+    let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`)
+    
+    if (toDelete){
+      this.goals.splice(index,1)
+      this.alertService.alertMe("The goal has been deleted")
+    }
+  }
+}
+constructor(goalService:GoalService, alertService:AlertService) {
+  this.goals = goalService.getGoals()
+  this.alertService = alertService;
+
+ }  
+ 
+ ngOnInit(): void {
+   interface ApiResponse {
+     author: string;
+     quote: string;
+   }
+
+    return this.http.get (<ApiResponse>("http://quotes.stormconsultancy.co.uk/random.json")
+    .subscribe((data: { author: string; quote: string; })=>{
+      // Succesful API request
+      this.quote = new Quote(data.author, data.quote)
+    }),
+
+
+  }
